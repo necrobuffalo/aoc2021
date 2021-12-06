7618c72c2c3b451f3e068eb1868553c31603a3c0
Adam Barber <git@sev.sh>
2021-12-05 23:21:50 -0500

    day 2

---
+++ src/bin/day2.rs
@@ -0,0 +1,80 @@
+use std::fs;
+use std::str::FromStr;
+
+extern crate util;
+
+enum Direction {
+    Forward(usize),
+    Up(usize),
+    Down(usize),
+}
+
+impl FromStr for Direction {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split = s.split_once(" ").unwrap();
+        let amount = split.1.parse().unwrap();
+        match split.0 {
+            "forward" => Ok(Direction::Forward(amount)),
+            "up" => Ok(Direction::Up(amount)),
+            "down" => Ok(Direction::Down(amount)),
+            _ => Err("Invalid submarine instruction".to_string()),
+        }
+    }
+}
+
+fn main() {
+    // ingest file
+    let input = fs::read_to_string("input/day2.txt")
+        .expect("Failed to read file");
+    let input: Vec<&str> = input.lines().collect();
+
+    let mut directions = Vec::new();
+    for i in input {
+        directions.push(i.parse().unwrap())
+    }
+
+    part_a(&directions);
+    part_b(&directions);
+}
+
+fn part_a(input: &Vec<Direction>) {
+    let mut horizontal = 0;
+    let mut depth = 0;
+
+    for instruction in input {
+        match instruction {
+            Direction::Forward(n) => horizontal = horizontal + n,
+            Direction::Up(n) => depth = depth - n,
+            Direction::Down(n) => depth = depth + n,
+        }
+    }
+
+    println!("Final position: {}", horizontal);
+    println!("Final depth: {}", depth);
+
+    println!("Multiplied: {}", horizontal * depth);
+}
+
+fn part_b(input: &Vec<Direction>) {
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for instruction in input {
+        match instruction {
+            Direction::Forward(n) => {
+                horizontal = horizontal + n;
+                depth = depth + aim * n;
+            },
+            Direction::Up(n) => aim = aim - n,
+            Direction::Down(n) => aim = aim + n,
+        }
+    }
+
+    println!("p2 Final position: {}", horizontal);
+    println!("p2 Final depth: {}", depth);
+
+    println!("Multiplied: {}", horizontal * depth);
+}
